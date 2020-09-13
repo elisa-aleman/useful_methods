2e0e6440693b728b1a2f457b7b50bd682c631d2a
Elisa Claire Alemán Carreón <37046039+elisa-aleman@users.noreply.github.com>
2020-09-13 16:47:05 +0900

    removed some methods, moved some to other library

--- UsefulMethods.py
+++ UsefulMethods.py
@@ -7,9 +7,6 @@ import csv
 import numpy
 import pandas
 import pathlib
-import pickle
-from sklearn.model_selection import train_test_split
-import sqlite3
 from io import StringIO
 
 ######################################
@@ -134,7 +131,7 @@ def _make_gen(reader):
         yield b
         b = reader(1024*1024)
 
-def Line_count_rawgen(filepath, last_index_csv=True):
+def line_count_rawgen(filepath, last_index_csv=True):
     '''
     Fast way to count lines in a file
     https://stackoverflow.com/a/27518377
@@ -168,93 +165,6 @@ def flatten(container):
                 yield j
         else:
             yield i
-
-def OneHot(Y):
-    uniqueY = numpy.unique(Y)
-    oneHotY = numpy.zeros([Y.shape[0], uniqueY.shape[0]])
-    for num, i in enumerate(Y):
-        oneHotY[num][i] = 1
-    return oneHotY
-
-# get X, Y, test_x, test_y
-def ReadyData(data, test_size = 1000, do_shuffle=True):
-    x, y = zip(*data)
-    X, test_x, Y, test_y = train_test_split(x,y, test_size=test_size, shuffle=do_shuffle)
-    return X,Y,test_x,test_y
-
-#####################################
-####### Tensorflow methods ##########
-#####################################
-
-### To check the tensorboard log
-def print_log_instructions():
-    print("To be able to see Tensorboard in your local machine after training on a server")
-    print("    1. exit current server session")
-    print("    2. connect again with the following command:")
-    print("        ssh -L 16006:127.0.0.1:6006 -p [port] [user]@[server]")
-    print("    3. execute in terminal")
-    print("        tensorboard --logdir='{}'".format(MakeLogFile('', server=True)))
-    print("    4. on local machine, open browser on:")
-    print("        http://127.0.0.1:16006")
-
-# Test NN training output
-def getCurrentAverageError(model,test_x,test_y):
-    pred_y = [p[0] for p in model.predict(test_x)]
-    losses = [(i[0]-i[1])**2 for i in zip(pred_y,test_y)]
-    mean_square_man = numpy.average(losses)
-    av_error = mean_square_man**0.5
-    return av_error
-
-##########################################
-############## CSV and SQL ###############
-##########################################
-
-def Connect(dbname):
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    return conn,c
-
-def CSVcreateSQL(titles, dbname, tablename):
-    conn, c = Connect(dbname)
-    size = len(titles)
-    # titles_quotes = ["'{}'".format(i) for i in titles]
-    # titles_str = ", ".join(titles_quotes)
-    c.execute("CREATE TABLE {tn} ('{nf}' INTEGER PRIMARY KEY)".format(tn=tablename, nf=titles[0]))
-    for title in titles[1:]:
-        c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' INTEGER".format(tn=tablename, cn=title))
-        conn.commit()
-    conn.commit()
-
-def CSVtoSQL(titles, table, dbname, tablename='users'):
-    times = len(titles)/100
-    conn, c = Connect(dbname)
-    columns = titles
-    columns_quote = ["'{}'".format(i) for i in columns]
-    columns_str = ", ".join(columns_quote)
-    sentence = "?{}".format(",?"*(len(columns)-1))
-    sql = "INSERT INTO {} ({}) VALUES({})".format(tablename, columns_str, sentence)
-    ins = [tuple(row) for row in table]
-    c.executemany(sql,ins)
-    conn.commit()
-    conn.close()
-
-def SQLtoCSV(dbname):
-    # dbname = 'ctrip_db2.sqlite'
-    conn, c = Connect(MakeSQLpath(dbname))
-    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
-    tables = c.fetchall()
-    tables = [table[0] for table in tables]
-    for table in tables:
-        sql = u"SELECT * FROM {}".format(table)
-        c.execute(sql)
-        raw = c.fetchall()
-        columns = [des[0] for des in c.description]
-        log_file = MakeSQLpath(os.path.join('CSV',"{}.csv".format(table)))
-        strlog = u",".join(columns)
-        printLog(strlog, log_file)
-        for row in raw:
-            strlog = u",".join([u'"{}"'.format(item) if type(item) == type(u'') else u'{}'.format(item) for item in row])
-            printLog(strlog, log_file)
 
 if __name__ == '__main__':
     pass
