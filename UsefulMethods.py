e3c907fbc46c34a8c862697e9eb8e90c974a07bb
Elisa Claire Alemán Carreón <37046039+elisa-aleman@users.noreply.github.com>
2020-09-13 16:38:47 +0900

    updated help section, added more methods

--- UsefulMethods.py
+++ UsefulMethods.py
@@ -4,60 +4,152 @@ import os
 import sys
 import codecs
 import csv
-import scipy
 import numpy
+import pandas
+import pathlib
+import pickle
 from sklearn.model_selection import train_test_split
 import sqlite3
+from io import StringIO
 
 ######################################
 ###### Special printing methods ######
 ######################################
-#Go up a line in the terminal to print over something
-def up(): 
+
+def up():
+    '''
+    Go up a line in the terminal to print over something
+    '''
     # My terminal breaks if we don't flush after the escape-code
     sys.stdout.write('\x1b[1A')
     sys.stdout.flush()
 
-#Go down a line in the terminal (like print(""))
 def down(): 
+    '''
+    Go down a line in the terminal (like print(""))
+    '''
     # I could use '\x1b[1B' here, but newline is faster and easier
     sys.stdout.write('\n')
     sys.stdout.flush()
 
-#Print to terminal and to file at the same time
-def printSTDlog(strlog, log_file): 
+def print_STD_log(strlog, log_file): 
+    '''
+    Print to terminal and to file at the same time
+    '''
     with codecs.open(log_file, 'a', 'utf-8') as logf:
-            print(strlog)
+        print(strlog)
+        strlog+= "\n"
+        logf.write(strlog)
+
+def print_log(strlog, log_file):
+    '''
+    Print string to a file
+    '''
+    with codecs.open(log_file, 'a', 'utf-8') as logf:
+        strlog+= "\n"
+        logf.write(strlog)
+
+def print_list_to_log(strlist, log_file):
+    '''
+    Print list of strings to a file
+    '''
+    with codecs.open(log_file, 'a', 'utf-8') as logf:
+        for strlog in strlist:
             strlog+= "\n"
             logf.write(strlog)
 
-#Print to a file
-def printLog(strlog, log_file):
+def print_list_to_STD_log(strlist, log_file):
+    '''
+    Print list of strings to the terminal and to a file at the same time
+    '''
     with codecs.open(log_file, 'a', 'utf-8') as logf:
+        for strlog in strlist:
+            print(strlog)
             strlog+= "\n"
-            logf.write(strlog) 
+            logf.write(strlog)
+        
+def write_tuple_list_to_csv(ins_table, filepath):
+    '''
+    Write tuple list to CSV file
+    '''
+    with open(filepath,'w') as out:
+        csv_out=csv.writer(out, delimiter=',', quotechar='"', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
+        csv_out.writerows(ins_table)
 
+def append_tuple_to_csv(ins_tuple, filepath):
+    '''
+    Append tuple to CSV file
+    '''
+    with open(filepath,'a+') as out:
+        csv_out=csv.writer(out, delimiter=',', quotechar='"', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
+        csv_out.writerow(ins_tuple)
+        
 ######################################
 ###### Data organizing methods #######
 ######################################
 
-# Read a CSV file
-def readCSV(filename, titlesplit=True):
-    f = open(filename, 'rt', encoding='utf-8')
-    reader = csv.reader(f, delimiter = ',')
-    table = []
-    for row in reader:
-        table.append(tuple(row))
-    f.close()
-    if titlesplit:
-        titles = table[0]
-        table = table[1:]
-        return titles, table
-    else:
-        return table
+def tail(filepath, decode_utf8=True, with_head=True, pandas_read=True):
+    '''
+    Returns the last line in a file
+    '''
+    with open(filepath, "rb") as f:
+        first = f.readline()      # Read the first line.
+        f.seek(-2, 2)             # Jump to the second last byte.
+        while f.read(1) != b"\n": # Until EOL is found...
+            try:
+                f.seek(-2, 1)     # ...jump back the read byte plus one more.
+            except IOError:
+                f.seek(-1, 1)
+                if f.tell() == 0:
+                    break
+        last = f.readline()       # Read last line.
+    if decode_utf8:
+        if with_head:
+            first = first.decode('utf-8')
+        last = last.decode('utf-8')
+    if with_head:
+        last = first+last
+    if pandas_read:
+        last = [tuple(row) for row in csv.reader(StringIO(last), delimiter=',', quotechar='"', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)]
+        if with_head:
+            last = pandas.DataFrame([last[1]], columns=last[0])
+        else:
+            last = pandas.DataFrame(last)
+    return last
 
-#Get a word list text file (a word per line) into a list
-def ReadDict(filename):
+
+def head(filepath, decode_utf8=True):
+    '''
+    Returns the first line in a file
+    '''
+    with open(filepath, "rb") as f:
+        first = f.readline()
+    if decode_utf8:
+        first = first.decode('utf-8')
+    return first
+
+def _make_gen(reader):
+    b = reader(1024 * 1024)
+    while b:
+        yield b
+        b = reader(1024*1024)
+
+def Line_count_rawgen(filepath, last_index_csv=True):
+    '''
+    Fast way to count lines in a file
+    https://stackoverflow.com/a/27518377
+    '''
+    f = open(filepath, 'rb')
+    f_gen = _make_gen(f.raw.read)
+    if last_index_csv:
+        return sum( buf.count(b'\n') for buf in f_gen ) - 2
+    else:
+        return sum( buf.count(b'\n') for buf in f_gen )
+
+def read_dict(filename):
+    '''
+    Get a word list text file (a word per line) into a list
+    '''
     dictionary = []
     with open(filename, 'r') as thefile:
         for line in thefile:
@@ -66,8 +158,10 @@ def ReadDict(filename):
                 dictionary.append(nline)
     return dictionary
 
-#Make a list like [1,[2,3],[4,[5]]] into [1,2,3,4,5]
 def flatten(container):
+    '''
+    Make a list or tuple like [1,[2,3],[4,[5]]] into [1,2,3,4,5]
+    '''
     for i in container:
         if isinstance(i, (list,tuple)):
             for j in flatten(i):
