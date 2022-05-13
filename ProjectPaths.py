ee1f8336d54fac50037bd677dfcce831a274b275
elisa-aleman <elisa.claire.aleman.carreon@gmail.com>
2022-05-13 17:34:10 +0900

    Changed documentation to annotated arg types + docstring

--- ProjectPaths.py
+++ ProjectPaths.py
@@ -7,9 +7,10 @@ import pathlib
 ######## Paths ########
 #######################
 
-def make_log_file(filename):
+def make_log_file(filename:'str')->'str':
     '''
-    Make Log file path string
+    Make Log file path string under <project_folder>/logs/filename
+    And create the parent folder if it doesn't exist already 
     '''
     halfpath = os.path.join(os.path.dirname(os.getcwd()), 'logs')
     fullpath = os.path.join(halfpath, filename)
@@ -18,9 +19,10 @@ def make_log_file(filename):
     pathlib.Path(file_parent).mkdir(parents = True, exist_ok=True)
     return fullpath
 
-def make_data_path(filename):
+def make_data_path(filename:'str')->'str':
     '''
-    Make Data file path string
+    Make Data file path string under <project_folder>/data/filename
+    And create the parent folder if it doesn't exist already 
     '''
     halfpath=os.path.join(os.path.dirname(os.getcwd()), 'data')
     fullpath = os.path.join(halfpath, filename)
@@ -29,9 +31,10 @@ def make_data_path(filename):
     pathlib.Path(file_parent).mkdir(parents = True, exist_ok=True)
     return fullpath
 
-def make_keyword_path(filename):
+def make_keyword_path(filename:'str')->'str':
     '''
-    Make Keyword file path string
+    Make Keyword file path string under <project_folder>/keyword/filename
+    And create the parent folder if it doesn't exist already 
     '''
     halfpath = os.path.join(os.path.dirname(os.getcwd()), 'keywords')
     fullpath = os.path.join(halfpath, filename)
@@ -40,9 +43,10 @@ def make_keyword_path(filename):
     pathlib.Path(file_parent).mkdir(parents = True, exist_ok=True)
     return fullpath
 
-def make_model_path(filename):
+def make_model_path(filename:'str')->'str':
     '''
-    Make Model file path string 
+    Make Model file path string under <project_folder>/model/filename
+    And create the parent folder if it doesn't exist already 
     '''
     halfpath= make_data_path('models')
     fullpath = os.path.join(halfpath, filename)
@@ -50,6 +54,10 @@ def make_model_path(filename):
     file_parent = os.path.split(fullpath)[0]
     pathlib.Path(file_parent).mkdir(parents = True, exist_ok=True)
     return fullpath
+
+if __name__ == '__main__':
+    pass
+
 
 if __name__ == '__main__':
     pass
